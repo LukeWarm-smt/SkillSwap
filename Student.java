16eeeaeb1b92375b2e11a37880912c5c00b4410b
Eeeeepy <gianlucabruzzi2008@gmail.com>
2026-03-05 16:23:44 +0100

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+    private String name;
+    private int student_id;
+    private String email;
+    private double ratings;
+    private int rating_counts;
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public int getId() { return student_id; }
+    public double getRatings() { return ratings; }
+    public int getRatingsCount() { return rating_counts; }
+}
