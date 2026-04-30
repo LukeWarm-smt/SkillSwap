5772f090255449831cf9102d31226a7e2a04e924
LukeWarm-smt <s10514908y@majorana-giorgi.edu.it>
2026-04-30 15:42:47 +0200

    Add files via upload

--- SkillSwap/src/app/SkillSwap.java
+++ SkillSwap/src/app/SkillSwap.java
@@ -1,130 +1,22 @@
 package SkillSwap.src.app;
 
-import SkillSwap.src.domain.*;
-
-import java.io.IOException;
-
-import java.nio.file.Files;
-
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
-import java.util.Map;
-
-import java.util.Scanner;
-import java.util.Set;
-
-import javax.swing.plaf.nimbus.State;
-
-import SkillSwap.src.app.*;
-
 import SkillSwap.src.service.*;
-
 import SkillSwap.src.state.*;
+import java.util.Scanner;
 
 public class SkillSwap {
 
     public static void main(String[] args) {
 
-        StateSkillSwap state = new StateSkillSwap();
+        StateSkillSwap state;
+        try{
+            state= new StateSkillSwap();
+        } catch (IllegalArgumentException e){
+            System.err.println("Problem during State Creation");
+            return;
+        }
+        
         Scanner scanf = new Scanner(System.in);
-
-        int i = 0;
-
-        List<String> students;
-
-        try {
-
-            students = Files.readAllLines(Paths.get("SkillSwap/src/app/student.csv"));
-
-        } catch (IOException e) {
-
-            System.out.println("Impossible reading names file...");
-
-            return;
-
-        }
-
-        students.stream().forEach(studenty -> {
-            try {
-                Student student = new Student(studenty);
-                state.addStudent(student.getId(), student);
-            } catch (Exception e) {
-                System.out.println("Errore in studente " + studenty);
-            }
-        });
-
-        List<String> skills;
-
-        try {
-
-            skills = Files.readAllLines(Paths.get("SkillSwap/src/app/skills.csv"));
-
-        } catch (IOException e) {
-
-            System.out.println("Impossible reading names file...");
-
-            return;
-
-        }
-
-        skills.stream().forEach(skilly -> {
-            try {
-                Skill skill = new Skill(skilly);
-                state.addSkill(skill.getSkillId(), skill);
-            } catch (Exception e) {
-                System.out.println("Errore in skill " + skilly);
-            }
-        });
-
-        List<String> offers;
-
-        try {
-
-            offers = Files.readAllLines(Paths.get("SkillSwap/src/app/offers.csv"));
-
-        } catch (IOException e) {
-
-            System.out.println("Impossible reading names file...");
-
-            return;
-
-        }
-
-        offers.stream().forEach(offerty -> {
-            try {
-                Offer offer = new Offer(offerty);
-                state.addOffer(offer.getOfferId(), offer);
-            } catch (Exception e) {
-                System.out.println("Errore in offer " + offerty);
-            }
-        });
-
-        List<String> requests;
-
-        try {
-
-            requests = Files.readAllLines(Paths.get("SkillSwap/src/app/requests.csv"));
-
-        } catch (IOException e) {
-
-            System.out.println("Impossible reading names file...");
-
-            return;
-
-        }
-
-        requests.stream().forEach(requesty -> {
-            try {
-                Request request = new Request(requesty);
-                state.addRequest(request.getRequestId(), request);
-            } catch (Exception e) {
-                System.out.println("Errore in request " + requesty);
-            }
-        });
         ConsoleReportPrinter crp = new ConsoleReportPrinter(state);
         MatchingService Mservices = new MatchingService(state);
         while (true)
