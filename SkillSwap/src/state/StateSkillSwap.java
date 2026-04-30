6fdc51e5533665063914d7fda7bd50df63fdb38c
LukeWarm-smt <s10514908y@majorana-giorgi.edu.it>
2026-04-30 15:44:39 +0200

    Add files via upload

--- SkillSwap/src/state/StateSkillSwap.java
+++ SkillSwap/src/state/StateSkillSwap.java
@@ -1,7 +1,11 @@
 package SkillSwap.src.state;
 
 import SkillSwap.src.domain.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StateSkillSwap {
@@ -19,6 +23,53 @@ public class StateSkillSwap {
     public Map<String, Skill> getSkills() { return skillState; }
     public Map<String, Offer> getOffers() { return offerState; }
     public Map<String, Request> getRequest() { return requestState; }
+
+    public StateSkillSwap() throws IllegalArgumentException{
+        List<String> list;
+        try {
+            list = Files.readAllLines(Paths.get("SkillSwap/resources/csv/student.csv"));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File student not found");
+        }
+
+        for (String string : list) {
+            Student e = new Student(string);
+            addStudent(e.getId(), e);
+        }
+
+        try {
+            list = Files.readAllLines(Paths.get("SkillSwap/resources/csv/skills.csv"));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File skills not found");
+        }
+
+        for (String string : list) {
+            Skill e = new Skill(string);
+            addSkill(e.getSkillId(), e);
+        }
+
+        try {
+            list = Files.readAllLines(Paths.get("SkillSwap/resources/csv/offers.csv"));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File offers not found");
+        }
+
+        for (String string : list) {
+            Offer e = new Offer(string);
+            addOffer(e.getOfferId(), e);
+        }
+
+        try {
+            list = Files.readAllLines(Paths.get("SkillSwap/resources/csv/requests.csv"));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File requests not found");
+        }
+
+        for (String string : list) {
+            Request e = new Request(string);
+            addRequest(e.getRequestId(), e);
+        }
+    }
 
     //debug stuff
     @Override
